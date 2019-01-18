7cfa74baf383dba3e7820ae14a0c880752432471
owencyr <cyroc@dukes.jmu.edu>
2019-01-18 12:51:47 -0500

    pseudocode for remaining features, started handleDelete implementation

--- index.js
+++ index.js
@@ -25,18 +25,40 @@ function renderTheList() {
 function handleNewEntries() {
   // this function will handle inputs from users in the submit form at the top
   // from #js-shopping-list-form
+  // listen to form submission and grab text input
+  // reformat raw text input to be object ready for STORE
+  // push to STORE
+  // send render function
   console.log('handleNewEntries ran');
 }
 
 function handleChecked() {
   // this function will handle checking items on the shopping list, when the 'check' button is toggled
+  //listen to 'CHECK' button click
+  // traverse to the shopping item class to get the index in STORE
+  // use index to toggle associated checked property in STORE
+  // send render function
   console.log('handleChecked ran');
 }
 
 function handleDelete() {
   // this function will handle deletion of items from shopping list when 'delete' button is clicked
+  // listen to "DELETE" button click
+  let shoppingItem = '';
+  $('.shopping-list').on('click', '.shopping-item-delete', (event) => {
+    shoppingItem = $(event.currentTarget).parents('.shopping-item-controls').siblings('.shopping-item'); 
+  };
+  // traverse to the shopping item class to get the index in STORE
+  let itemIndex = getitemIndexfromSTORE();
+  STORE[indexofshoppingitem].remove();
+  // use index to remove associated checked property in STORE
+
+  // send render function
+  renderTheList();
   console.log('handleDelete ran');
 }
+
+function getitemIndexfromSTORE(
 
 // this function calls all of the function stubs to run together on page load
 function handleShoppingList() {
