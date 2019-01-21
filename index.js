c801816c40917f933c02c1da5f007146f7194f2d
owencyr <cyroc@dukes.jmu.edu>
2019-01-21 15:33:19 -0500

    search and hide checked working, STORE now controls more elements of current state, and developing edit feature

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 /* global $ */
 'use strict';
 
+// URGENT missing features:
+// #1 hideChecked
+// #2 inline edit
+
+
 // issues:
 
 // #1 accepts no text input, need to reject ''
@@ -16,6 +21,9 @@
 //    add item STATE should update to clear text field
 //    search item STATE should either retain text field input if there are no matches, or clear if there are?
 
+// #4 search currently only matches ===, ie. items: oranges search will find 'oranges' but not 'orange'
+
+// #5 add display for no search match
 
 
 // SHOPPING LIST APPLICATION
@@ -25,21 +33,25 @@
 // to support multiple page states we want to render
 
 
-const STORE = [
-  {name: 'apples', checked: false},
-  {name: 'oranges', checked: false},
-  {name: 'milk', checked: true},
-  {name: 'bread', checked: false}
-];
-// searchResults: {};
-// hideCompleted: false;
+const STORE = {
+  items: [
+    {name: 'apples', checked: false, editable: false},
+    {name: 'oranges', checked: false, editable: false},
+    {name: 'milk', checked: true, editable: false},
+    {name: 'bread', checked: false, editable: false}
+  ],
+  searchResults: {},
+  hideChecked: false,
+};
 
-function renderShoppingList(database) {
+function renderShoppingList(database, subset='items') {
   // this function will render the shopping list in the DOM based on current STORE
   // render the STORE items in html by using a template function
-  let shoppingListHTML = createListItemHTML(database);
+  // other features have option of storing their state specific data in STORE, then render
+  // based on that feature driven state.
+  let shoppingListHTML = createListItemHTML(database[subset]);
 
-  // reinitialize text field, ad hoc implementation and should be changed
+  // clear text field, ad hoc implementation and should be changed
   document.getElementById('js-shopping-list-entry').value = '';
 
   //insert html to parent shopping list element
@@ -52,18 +64,19 @@ function createListItemHTML(database) {
 }
 
 function shoppingItemTemplate(item, itemIndex) {
-  return `
+  console.log(item.name, item.editable);
+  return (STORE.hideChecked && item.checked) ? '': `
   <li class="js-item-index-element" data-item-index="${itemIndex}">
-  <span class="shopping-item js-shopping-item ${item.checked ? 'class = shopping-item__checked' : ''}">${item.name}</span>
-  <div class="shopping-item-controls">
-    <button class="shopping-item-toggle js-item-toggle">
-        <span class="button-label">check</span>
-    </button>
-    <button class="shopping-item-delete js-item-delete">
-        <span class="button-label">delete</span>
-    </button>
-  </div>
-</li>`;
+    <span class="shopping-item js-shopping-item ${item.checked ? 'shopping-item__checked' : ''}"> ${item.editable ? `<input type=text class="shopping-item-edit js-item-edit" placeholder=${item.name}></input>` : item.name} </span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle js-item-toggle">
+          <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete js-item-delete">
+          <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`;
 }
 
 function handleAddShoppingItem() {
@@ -71,6 +84,7 @@ function handleAddShoppingItem() {
     e.preventDefault();
     // convert raw text input into a shopping item ready for STORE
     let shoppingItem = formatTextInput(getTextInput());
+    
     // add newly converted item to STORE
     addToDatabase(shoppingItem);
     // render modified DOM
@@ -79,27 +93,28 @@ function handleAddShoppingItem() {
   });
   console.log('Ready to add to shopping list');
 }
-//current issue: difference between submit from text field/button throws off creating object correctly
+
+// cannot specify trigger id/class of .submit(), in contrast to .on()
 function handleSearchShoppingList() {
   $('#js-shopping-list-form').on('click', '.js-search-shopping-list', function(e) { // listen to form submission
     e.preventDefault();
     // retrieve raw textinput
-    let searchTerm = getTextInput();
+    let searchTerm = getTextInput('.js-shopping-list-entry');
     // use filter to find matching names in STORE
-    let searchResults = searchDatabase(searchTerm);
+    STORE.searchResults = searchDatabase(searchTerm);
     // render only the filtered list
-    renderShoppingList(searchResults);
-    console.log('found', searchResults);
+    renderShoppingList(STORE, 'searchResults');
+    console.log('found', STORE.searchResults);
   });
   console.log('Ready to search shopping list');
 }
 
-function getTextInput() {
-  return $('.js-shopping-list-entry').val(); // grab raw text input
+function getTextInput(textBox) {
+  return $(textBox).val(); // grab raw text input
 }
 
 function searchDatabase(searchTerm) { //recieve raw text str and filter STORE based on match to .name property
-  return STORE.filter(item => item.name === searchTerm);
+  return STORE.items.filter(item => item.name === searchTerm);
 }
 
 function formatTextInput(itemName) {
@@ -107,7 +122,7 @@ function formatTextInput(itemName) {
 }
 
 function addToDatabase(item) {
-  STORE.push(item);
+  STORE.items.push(item);
 }
 
 function handleChecked() {
@@ -127,21 +142,18 @@ function handleChecked() {
 }
 
 function toggleProperty (index, prop) {
-  return STORE[index][prop] = !STORE[index][prop];
+  return STORE.items[index][prop] = !STORE.items[index][prop];
 }
 
 
 function handleDelete() {
   // this function will handle deletion of items from shopping list when 'delete' button is clicked
   // listen to "DELETE" button click
-  let shoppingItem = '';
-  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
-    shoppingItem = $(event.currentTarget).parents('.shopping-item-controls').siblings('.shopping-item'); 
-  
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {  
     // traverse to the shopping item class to get the index in STORE
-    let itemIndex = getitemIndexfromElement(shoppingItem); //eventually combine line event listener and this line traversal
+    let itemIndex = getitemIndexfromElement(event.target);
     // use index to remove associated checked property in STORE
-    STORE.splice(itemIndex, 1); //remember that splice will cut all elements unless it is given a limit argument. here we splice 1 element, inclusive starting from the element that splice searched for
+    STORE.items.splice(itemIndex, 1); //remember that splice will cut all elements unless it is given a limit argument. here we splice 1 element, inclusive starting from the element that splice searched for
     // send render function
     renderShoppingList(STORE);    
   });
@@ -155,7 +167,41 @@ function getitemIndexfromElement(item){
 }
 
 // checkboxes receive 'change' and 'input' events
+function handleHideChecked() {
+  // trigger on change of checkbox
+  $('#js-filter-checkboxes').on('change', '.js-hide-checked', () => {
+    // toggle boolean in STATE to hidechecked on next render
+    STORE.hideChecked = !STORE.hideChecked;
+    // render modified STORE
+    renderShoppingList(STORE);
+  });
+  console.log('Ready to hide checked...');
+}
 
+function handleEdit() {
+  // trigger on click .js-shopping-item
+  // toggle "editable" STORE.items property
+  // render modified STORE
+  $('.shopping-list').on('dblclick', '.js-shopping-item', function(e) {
+    let itemIndex = getitemIndexfromElement(e.target);
+    STORE.items[itemIndex].editable = !STORE.items[itemIndex].editable;
+    // STORE.items[$('.js-shopping-item').val()].editable = !STORE[$('.js-shopping-item').val()].editable;
+    renderShoppingList(STORE);
+    console.log('item now editable');
+    // after rendering item with text box instead of STORE.item.name text
+    // place a listener on the text box submission
+    // text box event listener is destroyed when there is no editable box from this function 
+  });
+  $('.shopping-list').on('submit', '.js-item-edit', function(e) {
+    e.preventDefault();
+    let itemIndex = getitemIndexfromElement(e.target);
+    // set the STORE.item.name to the captured text input
+    let newName = getTextInput('.js-item-edit');
+    STORE.items[itemIndex].name = newName;
+    renderShoppingList(STORE);
+  });
+  console.log('Ready to edit items');
+}
 
 
 
@@ -166,6 +212,8 @@ function handleShoppingList() {
   handleSearchShoppingList(); //search shopping-item in STORE
   handleChecked(); //toggling shopping item strikethrough button
   handleDelete(); //removing shopping items button
+  handleHideChecked();
+  handleEdit();
 }
 
 // calling the 'handle shopping list' function when the page loads
